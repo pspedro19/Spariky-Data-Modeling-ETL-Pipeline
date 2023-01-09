47c3f51cae6421fff33833975afbc922a1b5ee7e
Pedro Perez <96pedroelias96@gmail.com>
2023-01-09 12:57:17 -0500

    Changes due to feedback

--- etl.py
+++ etl.py
@@ -6,6 +6,18 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
+
+    """This function process a song file and insert the song and artist data into our database. The function takes in two arguments:
+
+        #cur: a cursor object for interacting with the database
+        #filepath: a string representing the file path of the song file
+
+    #This function reads in the song file using the pd.read_json() function
+    #and stores the data in a Pandas DataFrame called df.
+    # It then extracts the relevant data for the song and artist records from the DataFrame
+    # and inserts them into the database using the cur.execute() function and the provided SQL insert statements song_table_insert and artist_table_insert."""
+
+
     # open song file
     df = pd.read_json(filepath, lines=True)
 
@@ -20,6 +32,17 @@ def process_song_file(cur, filepath):
 
 
 def process_log_file(cur, filepath):
+
+    """This function takes in two arguments:
+
+            cur: a cursor object for interacting with the database
+            filepath: a string representing the file path of the JSON folders file
+
+        This function processes a log file containing user activity data and inserts data about the users,
+        time, and songplays into the database. It reads in the log file, 
+        filters the data to include only certain user actions, converts timestamps to datetime format,
+        and creates and inserts records for the time, users, and songplays. It also queries the song and artist tables to obtain data about the songs that were played."""
+
     # open log file
     df =  pd.read_json(filepath, lines=True)
 
@@ -35,7 +58,7 @@ def process_log_file(cur, filepath):
     time_df = pd.DataFrame(dict(zip(column_labels, time_data)))
     time_df['start_time'] = pd.to_datetime(time_df['start_time'], unit='ms')
 
-
+    #this for funciton quey all rows to insert it in the time table
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
@@ -62,10 +85,27 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = [index+1, row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent]
+        songplay_data = [row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent]
+
         cur.execute(songplay_table_insert, songplay_data)
 
 def process_data(cur, conn, filepath, func):
+    """
+    cur: a cursor object for interacting with the database
+    conn: a connection object for the database
+    filepath: a string representing the file path of the directory containing the data files
+    func: a function that processes a single data file and inserts the data into the database
+
+
+    This function first retrieves a list of all the files in the specified directory with the .json file extensio
+    using os.walk() and glob.glob(). It then prints out the total number of files found.
+
+    Next, it iterates over the list of files and passes each file to the func function for processing.
+     It also commits the changes to the database after each file is processed and prints out the number of files
+      that have been processed out of the total number of files.
+    """
+
+
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
@@ -85,6 +125,22 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
+
+
+    """
+    The main function processes data stored in JSON files in the song_data
+    and log_data directories and loads the data into a PostgreSQL database
+    using the psycopg2 library and the process_song_file and process_log_file functions.
+    The process_song_file function reads in a JSON file containing song data, 
+    extracts data about songs and artists, and inserts the data into the database.
+   
+    The process_log_file function reads in a JSON file containing log data, 
+    filters the data, converts timestamps to datetime format, and creates and inserts records for the time,
+    users, and songplays into the database. It also queries the song and artist tables to obtain data about
+    the songs that were played and inserts records for songplays into the database.
+    """
+
+
     conn = psycopg2.connect("host=localhost dbname=sparkifydb user=postgres password=password")
     cur = conn.cursor()
 
--- sql_queries.py
+++ sql_queries.py
@@ -69,8 +69,8 @@ CREATE TABLE IF NOT EXISTS time(
 # INSERT RECORDS
 
 songplay_table_insert = ("""
-INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 ON CONFLICT (songplay_id)
 DO NOTHING
 """)
@@ -79,7 +79,7 @@ user_table_insert = ("""
 INSERT INTO users (user_id, first_name, last_name, gender, level)
 VALUES (%s, %s, %s, %s, %s)
 ON CONFLICT (user_id)
-DO NOTHING
+DO UPDATE SET level=EXCLUDED.level;
 """)
 
 song_table_insert = ("""
